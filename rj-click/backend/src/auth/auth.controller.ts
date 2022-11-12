0a1af3dcefc678ca5218f25224a8c79841881cc7
Alexander Alves <vendas77.rj@gmail.com>
2022-11-12 09:31:00 -0300

    Atualizando os arquivos da pasta auth

--- rj-click/backend/src/auth/auth.controller.ts
+++ rj-click/backend/src/auth/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly usersService: UsersService,
-    private readonly jwtService: JwtService) {}
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post()
+  async auth(@Body() authDto: AuthDto) {
+    return this.authService.login(authDto)
+  }
 }
--- rj-click/backend/src/auth/auth.service.ts
+++ rj-click/backend/src/auth/auth.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(username) 
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (user && isMatch) {
+      const { password, ...result } = user 
+      return result 
+    }
+    return null
+  }
+
+  async login(authDto: AuthDto) {
+    const { email } = authDto 
+    const payload = { username: email }
+    return { acess_token: this.jwtService.sign(payload) }
+  }
+}
